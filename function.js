39be9b0e6f7b3b60b27797de5d68a8ac37cd39ef
big-choi <bos33210@naver.com>
2023-08-17 16:40:35 +0900

    - narrowing - assertion

---
+++ function.js
@@ -0,0 +1,21 @@
+function 내함수(x) {
+    if (typeof x === 'string') {
+        // x의 타입이 string 이면
+        return x + '1';
+    }
+    else {
+        return x + 1;
+    }
+}
+// assertion 문법
+function 내함수2(x) {
+    var array = [];
+    array[0] = x;
+    // 용도
+    // 1. Narrowing
+    // 2. 어떤 타입이 들어올지 확실할 때.
+    // 내함수2('123') -> assertion 문법을 쓰면 캐치 못함.
+    // 왜 타입에러가 나는지 모르는 상황에서 임시 에러해결용
+    // 어떤 타입이 들어올지 확실히 알고 있는데 컴파일러 에러가 방해할 때
+}
+내함수2(123);
